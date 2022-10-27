95231a0e0f4bd6ed17d0a5eaab83a17690f8707d
Mykola Maliarenko <kcintes7@gmail.com>
2022-10-27 17:34:09 +0200

    Остання вправа

--- lesson_3_homework/chetverta_vprava.py
+++ lesson_3_homework/chetverta_vprava.py
@@ -1,14 +1,5 @@
 rik = int(input("Уведіть рік, який вас цікавить :"))
-c = rik % 4
-b = rik % 100
-a = rik % 400
-if 1900 < rik < 1000000:
-    print("Триває обчислення")
-elif c == 0:
-    print("Триває обчислення")
-elif b == 0:
-    print("Триває обчислення")
-elif a == 0:
-    print("This is leep year")
+if 1900 < rik < 1000000 and rik % 4 == 0 and rik % 100 != 0 or rik % 400 == 0:
+    print("Цей рік є високосний ")
 else:
-    print("This is not leep year")
+    print("Не відповідає умовам задачи")
